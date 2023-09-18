37da6c8d67bf974ddd0ad3bba86238a030ec521c
Dav1200 <davinder.singh@city.ac.uk>
2023-09-18 12:14:09 +0100

    Longest Common Prefix

--- leetCode/src/Main.java
+++ leetCode/src/Main.java
@@ -71,10 +71,60 @@ public class Main {
         }
         return total;
     }
-    ;
+
+
+
+    public static String longestCommonPrefix(String[] strs) {
+//saved last comapre with next
+        String longest = "";
+        String prefix = "";
+
+        if(strs.length ==1) {
+            return strs[0];
+        }
+int le = 0;
+    for(int i = 0 ; i < strs.length;i++){
+
+        if(longest.length() < strs[i].length()){
+            le = longest.length();
+        }
+        else {
+            le = strs[i].length();
+        }
+        if(i == 0) {
+            longest = strs[i];
+            continue;
+        }
+
+        prefix = "";
+
+
+
+
+        for(int j = 0 ;j<le;j++ ){
+            if(longest.charAt(j) == strs[i].charAt(j))
+            {
+
+         prefix = prefix + longest.charAt(j);
+            }
+            else break;
+        }
+        longest = prefix;
+
+    }
+
+    return prefix;
+
+    }
+
 
     public static void main(String[] args) {
       //  System.out.println(romanToInt("IXX"));
+
+        //String[] test = new String[1];
+        //test[0] = "a";
+
+        //System.out.println(longestCommonPrefix(test));
 
 
 
