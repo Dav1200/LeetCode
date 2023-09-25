0fcb56b5913c96863a9e6c05d9c34739b44fb6c4
Dav1200 <davinder.singh@city.ac.uk>
2023-09-25 11:42:55 +0100

    Update

---
+++ src/lengthOfLastWord.java
@@ -0,0 +1,16 @@
+public class lengthOfLastWord {
+
+    public static  int lengthOfLastWord(String s) {
+
+
+        //trim all extra spaces then substring from the last occurrence of ' '  and get the length of that
+        s = s.trim();
+        return s.substring(s.lastIndexOf(' ')+1).length();
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(lengthOfLastWord("   fly me   to   the moon  "));
+    }
+}
---
+++ src/plusOne.java
@@ -0,0 +1,47 @@
+public class plusOne {
+
+    public  static  int[] plusOne(int[] digits) {
+
+
+        if (digits[digits.length - 1] != 9) {
+            digits[digits.length - 1] = digits[digits.length - 1] + 1;
+            return digits;
+        }
+
+        boolean all9 = true;
+        int changeIndex = 0;
+
+        for(int i = digits.length-1; i>=0;i--){
+
+            if(digits[i] != 9){
+                all9 =false;
+                changeIndex = i;
+                break;
+            }
+        }
+        int[] b = new int[digits.length];
+        if(all9){
+            int[] a = new int[digits.length+1];
+            a[0] = 1;
+            return a;
+        }
+
+        else
+            for(int i =0;i<=changeIndex;i++){
+                b[i] = digits[i];
+
+                if(i == changeIndex){
+                    b[i] = b[i] +1;
+                }
+            }
+            return b;
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] array = {8,9,9,9};
+        System.out.println(plusOne(array));
+
+    }
+}
