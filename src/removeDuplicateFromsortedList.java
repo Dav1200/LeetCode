549faced4d28d9673ff82f7e92e4718348a8e354
Dav1200 <davinder.singh@city.ac.uk>
2023-09-25 14:26:36 +0100

    Update

---
+++ src/removeDuplicateFromsortedList.java
@@ -0,0 +1,11 @@
+public class removeDuplicateFromsortedList {
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/sqrt.java
@@ -0,0 +1,27 @@
+public class sqrt {
+
+    public static  int mySqrt(int x) {
+
+
+        if(x==1){
+            return 1;
+        }
+        for(long i = 1 ;i <= x;i++){
+
+            if(i*i == x){
+                return (int)i;
+            }
+            if(i*i >x){
+                return (int)i-1;
+
+            }
+        }
+        return 0;
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(mySqrt(2147483647));
+    }
+}
