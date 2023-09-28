cd7947ca7757765dd8f5d68fa319dbeeece1e850
Dav1200 <davinder.singh@city.ac.uk>
2023-09-28 18:21:57 +0100

    update

--- src/practice.java
+++ src/practice.java
@@ -1,15 +1,14 @@
 public class practice {
 
-    public static String power2(int num){
-        if(num ==0){
+    public static String power2(int num) {
+        if (num == 0) {
             return "false";
         }
 
-        while (num!=2){
-            if(num%2 == 0){
-                num = num /2;
-            }
-            else {
+        while (num != 2) {
+            if (num % 2 == 0) {
+                num = num / 2;
+            } else {
                 return "false";
             }
 
@@ -17,11 +16,12 @@ public class practice {
         return "true";
 
     }
-    public static String stringMerge(String str){
+
+    public static String stringMerge(String str) {
 
         String[] strList = str.split("\\*");
         StringBuilder combinedString = new StringBuilder();
-        for(int i = 0; i < strList[0].length();i++){
+        for (int i = 0; i < strList[0].length(); i++) {
             combinedString.append(strList[0].charAt(i));
             combinedString.append(strList[1].charAt(i));
 
@@ -31,44 +31,79 @@ public class practice {
 
     }
 
-    public static String swapCase(String str){
+    public static String swapCase(String str) {
         StringBuilder newString = new StringBuilder();
-        for(int i = 0; i < str.length();i++)
-        {
-            if(Character.isLowerCase(str.charAt(i))){
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isLowerCase(str.charAt(i))) {
                 newString.append(Character.toUpperCase(str.charAt(i)));
-            }
-            else {
+            } else {
                 newString.append(Character.toLowerCase(str.charAt(i)));
             }
         }
-return newString.toString();
+        return newString.toString();
 
     }
-public static String snakeCase(String str){
-        str = str.replaceAll("\\p{Punct}","");
+
+    public static String simpleSymbols(String str) {
+        boolean seen = false;
+        boolean seenletter = false;
+
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if (Character.isLetter(str.charAt(i)) && !seen) {
+                return "false";
+            }
+            if (str.charAt(i) == '+') {
+                seen = true;
+            }
+            if (seen) {
+                if (Character.isLetter(str.charAt(i))) {
+                    seenletter = true;
+                }
+            }
+            if (seen && seenletter) {
+                if (str.charAt(i) == '+') {
+                    seen = false;
+                }
+            }
+        }
+
+        if (seen) {
+            return "false";
+        }
+        return "true";
+
+    }
+
+    public static String snakeCase(String str) {
+        str = str.replaceAll("\\p{Punct}", "");
         str = str.trim();
-        str = str.replaceAll(" ","_");
+        str = str.replaceAll(" ", "_");
         str = str.toLowerCase();
         return str;
 
 
-}
-    public static String superincreasing(int[] arr){
+    }
+
+    public static String simpleEvens(Integer num) {
+        
+
+    }
+
+    public static String superincreasing(int[] arr) {
         int sum = 0;
 
-        for(int i = 0; i < arr.length;i++){
-            if(arr[i]>sum){
-                sum+= arr[i];
-            }
-
-
-            else{
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > sum) {
+                sum += arr[i];
+            } else {
                 return "false";
             }
         }
         return "true";
     }
+
     public static void main(String[] args) {
 
         //String result = stringMerge("abc1*kyoo");
@@ -81,9 +116,12 @@ public static String snakeCase(String str){
         //System.out.println(result1);
 
         //var result1 = snakeCase("Revolt& is& the right of the people");
-       // System.out.println(result1);
+        // System.out.println(result1);
 
-        var result1 = simpleSymbols("=+e++r+f+v+");
+        // var result2 = simpleSymbols("=+e++r+ff+v+");
+        //System.out.println(result2);
+
+        String result1 = simpleEvens(222252);
         System.out.println(result1);
     }
 }
