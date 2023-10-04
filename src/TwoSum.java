1f7e2a8dba1280237079a676e9222fdc52b8f32c
Dav1200 <davinder.singh@city.ac.uk>
2023-10-04 22:37:59 +0100

    ValidPalindrome Phrase

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -12,11 +12,10 @@ public class TwoSum {
         }
         return  false;
     }
-
+    
     public static void main(String[] args) {
 
         int [] nums = {1,2,3,4,5,6};
-
         System.out.println(twoSum(222,nums));
     }
 }
