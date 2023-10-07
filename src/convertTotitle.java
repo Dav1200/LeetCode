6c125b5f349bee7e7e2c6f39e5baceeb24ddf2de
Dav1200 <davinder.singh@city.ac.uk>
2023-10-07 17:43:14 +0100

    convertToTitle

--- src/convertTotitle.java
+++ src/convertTotitle.java
@@ -1,2 +1,29 @@
 public class convertTotitle {
+
+
+    public static String cconvertToTitle(int columnNumber) {
+
+        StringBuilder a = new StringBuilder();
+        while (columnNumber > 0) {
+            int reminder = columnNumber % 26;
+            if (reminder == 0) {
+                reminder = 26;
+            }
+            char add = 65;
+            add += reminder - 1;
+            a.append(add);
+            columnNumber -= reminder;
+            columnNumber /= 26;
+        }
+
+
+        return a.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+
+
+        System.out.println(cconvertToTitle(52));
+
+    }
 }
