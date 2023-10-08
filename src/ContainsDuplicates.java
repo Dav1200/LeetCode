0048d5585bf3c507464bf9ca3c630e226fea385e
Dav1200 <davinder.singh@city.ac.uk>
2023-10-08 22:08:59 +0100

    Check if contains duplicate in array

---
+++ src/ContainsDuplicates.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicates {
+
+    public static boolean containsDuplicate(int[] nums) {
+        if (nums.length == 0 || nums.length == 1) {
+            return false;
+        }
+        HashMap<Integer, Integer> dupTable = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (dupTable.containsKey(nums[i])) {
+                dupTable.put(nums[i], dupTable.get(nums[i]) + 1);
+            } else {
+                dupTable.put(nums[i], 1);
+            }
+        }
+
+        for (Map.Entry<Integer, Integer> map : dupTable.entrySet()) {
+            if (map.getValue() > 1) {
+                return true;
+            }
+
+        }
+        return false;
+
+    }
+}
