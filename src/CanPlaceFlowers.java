b6bf990b52308fc1b9131d52ccc48c127540465d
Dav1200 <davinder.singh@city.ac.uk>
2023-10-18 10:37:54 +0100

    Can plce flower unoptimised solution

--- src/CanPlaceFlowers.java
+++ src/CanPlaceFlowers.java
@@ -2,7 +2,9 @@ public class CanPlaceFlowers {
 
     public static boolean canPlaceFlowers(int[] flowerbed, int n) {
 
-
+        if(n ==0){
+            return true;
+        }
         if (flowerbed.length == 1) {
             if (flowerbed[0] == 0 && n == 1) {
                 return true;
@@ -13,33 +15,33 @@ public class CanPlaceFlowers {
         }
 
         for (int i = 0; i < flowerbed.length; i++) {
-
-
-            if (i == 0) {
-                if (flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
+            if(n == 0){
+                return true;
+            }
+            if(i == 0){
+                if(flowerbed[i] == 0 && flowerbed[i+1] == 0){
                     flowerbed[i] = 1;
                     n--;
 
                 }
             }
 
-            if (i == flowerbed.length - 1) {
-                if (flowerbed[i] == 0 && flowerbed[i - 1] == 0) {
+            if(i == flowerbed.length-1){
+                if(flowerbed[i] == 0 && flowerbed[i-1] == 0){
                     flowerbed[i] = 1;
                     n--;
 
                 }
             }
 
-            if (i != 0 && i != flowerbed.length - 1) {
-                if (flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0 && flowerbed[i] == 0) {
-                    flowerbed[i] = 1;
+            if(i!=0 && i!= flowerbed.length-1){
+                if(flowerbed[i-1] == 0 && flowerbed[i+1] == 0 && flowerbed[i] == 0){
+                    flowerbed[i] =1;
                     n--;
                 }
-            }
-        }
+            }}
 
-        return n == 0;
+        return n==0;
 
     }
 
